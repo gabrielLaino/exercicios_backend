e3c7493bb90c4f9f478a5e9887b35782387d6bfb
Gabriel Laino Baieteiro <gabriel.baieteiro@grupoetapa.local>
2024-10-07 15:56:31 -0300

    SQL-1 para fixar 2

---
+++ SQL/SQL-1/para_fixar_2/db.sql
@@ -0,0 +1,7 @@
+INSERT INTO models (name, brand)
+VALUES ('CG 150 TITAN KS', 'Honda')
+
+INSERT INTO models (name, brand)
+VALUES ('Corolla 2.0 XEI', 'Toyota'),
+        ('Uno 1.0 EVO VIVACE 8V', 'Fiat'),
+        ('Fit 1.5 Twist', 'Honda');
